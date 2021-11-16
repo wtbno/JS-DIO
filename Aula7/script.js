e5317cc66feebceed3bad448e89378c9ef912bd6
wtbno <bruno_links@hotmail.com>
2021-11-16 20:19:56 -0300

    continuação

---
+++ Aula7/script.js
@@ -0,0 +1,23 @@
+/*var lista = ["Zelda", "Sekiro", "Resident Evil"]
+console.log(lista.length);
+lista.push("Pokemon");
+console.log(lista);
+lista.pop("Pokemon");
+console.log(lista);
+console.log(lista[1]);
+console.log(lista.join(" - ")); // Separa os elementos da lista de uma determinada forma*/
+
+var GPU = [{nome: "Radeon", modelo: "RX 5500 XT", RAM:"8gb", preco: 3500 }, 
+            {nome: "GeForce", modelo: "1660 TI", RAM:"6gb", preco: 2899 },
+            {nome: "Radeon", modelo: "RX 590", RAM:"8gb", preco: 1800 },
+            {nome: "Geforce", modelo: "RTX 3060", RAM:"12gb", preco: 4600 },
+        ]
+console.log(GPU);
+
+
+
+//var GPU = {nome: "Radeon", modelo: "RX 5500 XT"} //objetos Json
+//console.log(GPU);
+
+//push adiciona elemento a lista
+//pop remove o elemento da lista
---
+++ Aula8/aula8.js
@@ -0,0 +1,34 @@
+var d = new Date();
+alert(d.getDate());
+alert(d.getMinutes());
+alert(d.getSeconds());
+alert(d.getMilliseconds());
+
+/*var count;
+for (count=0; count <= 5; count++){
+    alert(count);
+};*/
+   
+
+
+
+/*Repetição
+var count = 0;
+while(count <= 5 ){
+    console.log(count);
+    alert(count)
+    count ++;
+};*/
+
+
+
+
+/*
+//Condicionais
+var idade = prompt("Qual a sua idade?");//prompt armazena o dado inserido pelo usuário
+if(idade >= 18 ){
+    alert("Maior de idade, acesso permitido");
+}else{
+    alert("Menor de idade, acesso negado!");
+};*/
+
